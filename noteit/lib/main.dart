a7f9d0ac7b06585792a29206bd3fd08038bef568
Sanika Chandode <sanu30chandode@gmail.com>
2021-10-26 00:53:44 +0530

    Transparent Appbar added

--- noteit/lib/main.dart
+++ noteit/lib/main.dart
@@ -7,35 +7,106 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  get color => null;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        extendBodyBehindAppBar: true,
+        appBar: AppBar(
+          title: const Text(
+            'APPS.inc',
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+        ),
         body: Center(
           child: Stack(
             children: <Widget>[
               Image.asset(
                 "assets/images/home_background.png",
                 fit: BoxFit.cover,
-                height: 781.1,
-                width: 400,
+                height: double.infinity,
+                width: double.infinity,
                 //alignment: Alignment.center,
               ),
               Container(
-                  alignment: Alignment(-0.05, -0.80),
+                  alignment: const Alignment(-0.05, -0.65),
                   child: const Text(
                     'Noteit',
                     style: TextStyle(
                       color: Colors.white,
                       fontWeight: FontWeight.normal,
-                      fontSize: 35.0,
-                      fontStyle: FontStyle.italic,
+                      fontSize: 78.0,
+                      fontFamily: 'Daniel',
                     ),
                   )),
             ],
           ),
         ),
-        drawer: const Drawer(),
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: [
+              // ignore: sized_box_for_whitespace
+              Container(
+                height: 130.0,
+                child: const DrawerHeader(
+                  decoration: BoxDecoration(
+                    color: Colors.deepPurple,
+                    shape: BoxShape.rectangle,
+                  ),
+                  child: Text(
+                    "NoteIt Drawer",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 25,
+                    ),
+                  ),
+                ),
+              ),
+              ListTile(
+                title: const Text('Alarms'),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                title: const Text('Calender'),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                title: const Text('Notes'),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                title: const Text('Drawing'),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                title: const Text('Quick Notes'),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                title: const Text('Diary'),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
