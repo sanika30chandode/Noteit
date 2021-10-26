89adde0b6b416690671b908e4b7446df143b5674
Premraj02 <premrajjadhav02@gmail.com>
2021-10-26 22:55:57 +0530

    adding font2

--- noteit/lib/main.dart
+++ noteit/lib/main.dart
@@ -10,7 +10,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(fontFamily: 'Daniel'),
       home: Scaffold(
         body: Center(
           child: Stack(
