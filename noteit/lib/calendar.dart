678282c93e4a52aec9223fcbb2a496b607db9828
Zodeporas06 <zodeporas06@gmail.com>
2021-10-29 23:08:32 +0530

    Added Reminders

--- noteit/lib/calendar.dart
+++ noteit/lib/calendar.dart
@@ -1,5 +1,6 @@
 import 'dart:ui';
 
+import 'package:asdf/event.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
@@ -10,66 +11,118 @@ class Calendar extends StatefulWidget {
 }
 
 class _CalendarState extends State<Calendar> {
+  late Map<DateTime, List<Event>> selectedEvents;
   CalendarFormat format = CalendarFormat.month;
   DateTime selectedDay = DateTime.now();
   DateTime focusedDay = DateTime.now();
 
+  TextEditingController _eventController = TextEditingController();
+
+  @override
+  void initState() {
+    selectedEvents = {};
+    super.initState();
+  }
+
+  List<Event> _getEventsfromDay(DateTime date) {
+    return selectedEvents[date] ?? [];
+  }
+
+  @override
+  void dispose() {
+    _eventController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("CALENDAR"),
-        titleTextStyle: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-        centerTitle: true,
-        backgroundColor: Colors.deepPurple[700],
-      ),
-      body: TableCalendar(
-        focusedDay: selectedDay,
-        firstDay: DateTime(2000),
-        lastDay: DateTime(2050),
-        calendarFormat: format,
-        onFormatChanged: (CalendarFormat _format) {
-          setState(() {
-            format = _format;
-          });
-        },
-        startingDayOfWeek: StartingDayOfWeek.sunday,
-        daysOfWeekVisible: true,
-        onDaySelected: (DateTime selectDay, DateTime focusDay) {
-          setState(() {
-            selectedDay = selectDay;
-            focusedDay = focusDay;
-          });
-          print(focusedDay);
-        },
-        selectedDayPredicate: (DateTime date) {
-          return isSameDay(selectedDay, date);
-        },
-        calendarStyle: CalendarStyle(
-          isTodayHighlighted: true,
-          selectedDecoration: BoxDecoration(
-            color: Colors.pink[300],
-            shape: BoxShape.circle,
+        appBar: AppBar(
+          title: Text("CALENDAR"),
+          titleTextStyle: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+          centerTitle: true,
+          backgroundColor: Colors.deepPurple[700],
+        ),
+        body: TableCalendar(
+          focusedDay: selectedDay,
+          firstDay: DateTime(2000),
+          lastDay: DateTime(2050),
+          calendarFormat: format,
+          onFormatChanged: (CalendarFormat _format) {
+            setState(() {
+              format = _format;
+            });
+          },
+          startingDayOfWeek: StartingDayOfWeek.sunday,
+          daysOfWeekVisible: true,
+          onDaySelected: (DateTime selectDay, DateTime focusDay) {
+            setState(() {
+              selectedDay = selectDay;
+              focusedDay = focusDay;
+            });
+            print(focusedDay);
+          },
+          selectedDayPredicate: (DateTime date) {
+            return isSameDay(selectedDay, date);
+          },
+          eventLoader: _getEventsfromDay,
+          calendarStyle: CalendarStyle(
+            isTodayHighlighted: true,
+            selectedDecoration: BoxDecoration(
+              color: Colors.pink[300],
+              shape: BoxShape.circle,
+            ),
+            selectedTextStyle: TextStyle(color: Colors.white),
+            todayDecoration: BoxDecoration(
+              color: Colors.deepPurple[400],
+              shape: BoxShape.circle,
+            ),
           ),
-          selectedTextStyle: TextStyle(color: Colors.white),
-          todayDecoration: BoxDecoration(
-            color: Colors.deepPurple[400],
-            shape: BoxShape.circle,
+          headerStyle: HeaderStyle(
+            formatButtonVisible: true,
+            titleCentered: true,
+            formatButtonShowsNext: false,
+            formatButtonDecoration: BoxDecoration(
+              color: Colors.deepPurple[400],
+              borderRadius: BorderRadius.circular(5.0),
+            ),
+            formatButtonTextStyle: TextStyle(
+              color: Colors.white,
+            ),
           ),
         ),
-        headerStyle: HeaderStyle(
-          formatButtonVisible: true,
-          titleCentered: true,
-          formatButtonShowsNext: false,
-          formatButtonDecoration: BoxDecoration(
-            color: Colors.deepPurple[400],
-            borderRadius: BorderRadius.circular(5.0),
-          ),
-          formatButtonTextStyle: TextStyle(
-            color: Colors.white,
-          ),
-        ),
-      ),
-    );
+        floatingActionButton: FloatingActionButton.extended(
+          onPressed: () => showDialog(
+              context: context,
+              builder: (context) => AlertDialog(
+                    title: Text("Add Reminder"),
+                    content: TextFormField(
+                      controller: _eventController,
+                    ),
+                    actions: [
+                      TextButton(
+                        child: Text("Cancel"),
+                        onPressed: () => Navigator.pop(context),
+                      ),
+                      TextButton(
+                          child: Text("Ok"),
+                          onPressed: () {
+                            if (_eventController.text.isEmpty) {
+                              Navigator.pop(context);
+                              return;
+                            } else {
+                              selectedEvents[selectedDay] = [
+                                Event(title: _eventController.text)
+                              ];
+                              Navigator.pop(context);
+                              _eventController.clear();
+                              return;
+                            }
+                          }),
+                    ],
+                  )),
+          label: Text("Add Reminder"),
+          icon: Icon(Icons.add),
+        ));
   }
 }
--- noteit/lib/main.dart
+++ noteit/lib/main.dart
@@ -10,7 +10,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      //title: "NoteIt Calendar",
       home: Calendar(),
     );
   }
